de7af7515007b7f17097565a3478c539dec17434
def47619 <def47619@gmail.com>
2023-10-17 01:22:16 +0900

    feat: ch05 : 밝기 조정 기능 구현

---
+++ ch05_image_brighness/04_stretch_histogram.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2
+
+
+def calcGrayHist(img):
+    channels = [0]
+    histSize = [256]
+    histRange = [0, 256]
+
+    hist = cv2.calcHist([img], channels, None, histSize, histRange)
+
+    return hist
+
+
+def getGrayHistImage(hist):
+    histMax = np.max(hist)
+
+    imgHist = np.full((100, 256), 255, dtype=np.uint8)
+    for x in range(256):
+        pt1 = (x, 100)
+        pt2 = (x, 100 - int(hist[x, 0] * 100 / histMax))
+        cv2.line(imgHist, pt1, pt2, 0)
+
+    return imgHist
+
+
+def histgoram_stretching():
+    src = cv2.imread('./data/hawkes.bmp', cv2.IMREAD_GRAYSCALE)
+
+    if src is None:
+        print('histogram_stretching: Image load failed!')
+        return
+
+    # gmin = float(np.min(src))
+    # gmax = float(np.max(src))
+    gmin, gmax, _, _ = cv2.minMaxLoc(src)
+
+    # histogram_stretching : (src(x, y)-Gmin)/(Gmax-Gmin) * 255
+    dst = ((src - gmin) * 255. / (gmax - gmin)).astype(np.uint8)
+
+    cv2.imshow('src', src)
+    cv2.imshow('srcHist', getGrayHistImage(calcGrayHist(src))) # 히스토그램 그래프를 담고 있는 영상 
+
+    cv2.imshow('dst', dst)
+    cv2.imshow('dstHist', getGrayHistImage(calcGrayHist(dst)))
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    histgoram_stretching()
